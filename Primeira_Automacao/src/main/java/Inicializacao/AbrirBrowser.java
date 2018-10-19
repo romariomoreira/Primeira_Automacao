8983a87013d7d09cddd0b23151db30f94add0382
Romario Silva <romariomoreiradasilva@hotmail.com>
2018-10-19 00:08:50 -0300

    Explicação de como encontrar o navegador.

--- Primeira_Automacao/src/main/java/Inicializacao/AbrirBrowser.java
+++ Primeira_Automacao/src/main/java/Inicializacao/AbrirBrowser.java
@@ -11,12 +11,15 @@ public class AbrirBrowser {
 	
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub]
-		System.setProperty("Chromedriver", "chromedriver.exe");
+		// Abaixo como é usado para passar o endereço da pasta onde está o navegador, caso esteja em pasta diferente do projeto.
+		//System.setProperty("Chromedriver", "C:/Users/romar/git/repository/Primeira_Automacao/chromedriver.exe");
 		
+		//Abaixo como é usado para passar o navegador quando estiver na mesma pasta do Projeto. 
+		System.setProperty("Chromedriver", "chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.google.com.br");
 		
+		driver.close();
 				
 		
 		
