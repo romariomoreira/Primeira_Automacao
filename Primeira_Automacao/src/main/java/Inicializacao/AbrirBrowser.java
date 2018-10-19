6f100bc8a6da37eadfc1387e7ab3d110b8b3df47
Romario Silva <romariomoreiradasilva@hotmail.com>
2018-10-18 23:36:14 -0300

    Inclusão das dependencia do Selenium Inclusão do Chrome Driver

--- Primeira_Automacao/src/main/java/Inicializacao/AbrirBrowser.java
+++ Primeira_Automacao/src/main/java/Inicializacao/AbrirBrowser.java
@@ -1,9 +1,22 @@
 package Inicializacao;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AbrirBrowser {
 
+	
+	private static WebDriver driver;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub]
+		System.setProperty("Chromedriver", "chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com.br");
+		
+		
+		
+		
 
 	}
 
